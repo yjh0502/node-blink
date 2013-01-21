fb99775758e93b8d4a60310fb46f2b7ee933e07b
Jihyun Yu <yjh0502@gmail.com>
2013-01-21 20:42:19 +0900

    implement CBIT decoding functions

--- test/test_cbit.js
+++ test/test_cbit.js
@@ -1,27 +1,96 @@
 var assert = require('assert'),
     cbit = require('../lib/cbit');
 
+var rand_iter = 100000;
+
 describe('cbit', function() {
     describe('decode_unsigned', function() {
         it('correctness', function() {
-            assert(cbit.decode_signed(new Buffer('40', 'hex')), 64);
-            assert(cbit.decode_signed(new Buffer('8040', 'hex')), 64);
-            assert(cbit.decode_signed(new Buffer('808040', 'hex')), 64);
-            assert(cbit.decode_signed(new Buffer('80808040', 'hex')), 64);
-            assert(cbit.decode_signed(new Buffer('8080808040', 'hex')), 64);
+            assert.equal(cbit.decode_unsigned(new Buffer('40', 'hex'), 0), 64);
+            assert.equal(cbit.decode_unsigned(new Buffer('8040', 'hex'), 0), 64);
+            assert.equal(cbit.decode_unsigned(new Buffer('808040', 'hex'), 0), 64);
+            assert.equal(cbit.decode_unsigned(new Buffer('80808040', 'hex'), 0), 64);
+            assert.equal(cbit.decode_unsigned(new Buffer('8080808040', 'hex'), 0), 64);
 
-            assert(cbit.decode_signed(new Buffer('a467', 'hex')), 4711);
+            assert.equal(cbit.decode_unsigned(new Buffer('a467', 'hex'), 0), 4711);
         });
     });
 
     describe('decode_signed', function() {
         it('correctness', function() {
-            assert(cbit.decode_signed(new Buffer('01', 'hex')), 1);
-            assert(cbit.decode_signed(new Buffer('40', 'hex')), -64);
-            assert(cbit.decode_signed(new Buffer('8040', 'hex')), 64);
-            assert(cbit.decode_signed(new Buffer('db19', 'hex')), -4711);
+            assert.equal(cbit.decode_signed(new Buffer('01', 'hex'), 0), 1);
+            assert.equal(cbit.decode_signed(new Buffer('40', 'hex'), 0), -64);
+            assert.equal(cbit.decode_signed(new Buffer('8040', 'hex'), 0), 64);
+            assert.equal(cbit.decode_signed(new Buffer('db19', 'hex'), 0), -4711);
 
-            assert(cbit.decode_signed(new Buffer('f880808000', 'hex')), -2147483648);
+            assert.equal(cbit.decode_signed(new Buffer('f880808000', 'hex'), 0), -2147483648);
+            assert.equal(cbit.decode_signed(new Buffer('87c5b49f62', 'hex'), 0), 2024607714);
+        });
+    });
+
+    describe('encode_unsigned', function() {
+        it('correctness', function() {
+            var buf = new Buffer(10);
+            for(var i = 0; i < rand_iter; i++) {
+                var max = 0x7FFFFFFF;
+                var rand_val = (Math.random() * max)|0;
+                var len = cbit.encode_unsigned(rand_val, buf, 0);
+                var decoded = cbit.decode_unsigned(buf, 0);
+
+                assert.equal(rand_val, decoded);
+            }
+        });
+
+        it('boundary', function() {
+            var tests = [];
+            for(var i = 0; i < 4; i++) {
+                var base = (1 << (i * 7));
+                tests.push(base-1);
+                tests.push(base);
+                tests.push(base+1);
+            }
+
+            var buf = new Buffer(10);
+            for(var i = 0; i < tests.length; i++) {
+                var val = tests[i];
+                var len = cbit.encode_unsigned(val, buf, 0);
+                var decoded = cbit.decode_unsigned(buf, 0);
+
+                assert.equal(val, decoded);
+            }
+        });
+    });
+
+    describe('encode_signed', function() {
+        it('correctness', function() {
+            var buf = new Buffer(10);
+            for(var i = 0; i < rand_iter; i++) {
+                var max = 0xFFFFFFFF;
+                var rand_val = (Math.random() * max)|0;
+                var len = cbit.encode_signed(rand_val, buf, 0);
+                var decoded = cbit.decode_signed(buf, 0);
+
+                assert.equal(rand_val, decoded);
+            }
+        });
+
+        it('boundary', function() {
+            var tests = [];
+            for(var i = 1; i < 4; i++) {
+                var base = (1 << (i * 7 - 1));
+                tests.push(base-1);
+                tests.push(base);
+                tests.push(base+1);
+            }
+
+            var buf = new Buffer(10);
+            for(var i = 0; i < tests.length; i++) {
+                var val = tests[i];
+                var len = cbit.encode_signed(val, buf, 0);
+                var decoded = cbit.decode_signed(buf, 0);
+
+                assert.equal(val, decoded);
+            }
         });
     });
 });
